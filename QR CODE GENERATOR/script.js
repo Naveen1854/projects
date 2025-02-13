e578f745b9f82da9a78f72d84f187fce99001496
Naveen1854 <111000019+Naveen1854@users.noreply.github.com>
2025-02-13 16:28:50 +0530

    Add files via upload

---
+++ QR CODE GENERATOR/script.js
@@ -0,0 +1,8 @@
+let input = document.getElementById("url")
+let btn = document.getElementById("qr_btn")
+btn.addEventListener("click",()=>{
+    let qrLink = `https://image-charts.com/chart?
+chs=150x150&cht=qr&chl=${input.value}`
+let qr_img = document.getElementById("qr_img")
+qr_img.src=qrLink
+})
